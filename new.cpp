e872faa40dfb88aea371ec43c49404fedb8d2d02
Sarthak Pandey <sarthakpandey6162@gmail.com>
2025-10-08 16:12:41 +0530

    Add files via upload

---
+++ new.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int x;
+    cout<<"Enter the number";
+    cin>>x;
+    cout<<"this is the inutted number"<<x<<endl;
+    return 0;
+}
